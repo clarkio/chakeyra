5493d07396c7342497813d7fc4ae2d00d0463549
clarkio <clarkio@users.noreply.github.com>
2020-11-01 10:47:03 -0500

    add streamer key capture with resulting indication

--- src/client/index.js
+++ src/client/index.js
@@ -2,15 +2,46 @@
 
 // https://hipsum.co/api/?type=hipster-centric&sentences=1
 
-const requestHipsumWords = new XMLHttpRequest();
-
-requestHipsumWords.open('GET', 'https://hipsum.co/api/?type=hipster-centric&sentences=1');
-
-requestHipsumWords.onload = function () {
-  console.log(this.responseText);
-  
-}
+let streamerWords = '';
+let streamerCharacters = '';
+let streamerCompletedCharacters = '';
+let chatWords = '';
+let streamerDone = false;
+let chatDone = false;
 
 function getWords() {
-  requestHipsumWords.send();
+  document.addEventListener('keydown', captureKey);
+  axios.get('https://hipsum.co/api/?type=hipster-centric&sentences=1')
+    .then(response => {
+      document.getElementById('streamer-words').innerHTML = response.data[0];
+      streamerWords = response.data[0];
+      chatWords = response.data[0];
+      streamerCharacters = streamerWords.split('');
+      chatCharacters = chatWords.split('');
+      document.getElementById('start').disabled = true;
+    });
+}
+
+function stop() {
+  document.removeEventListener('keydown', captureKey);
+  document.getElementById('start').disabled = false;
+}
+
+
+function captureKey(event) {
+  const key = event.key;
+  const streamerWordsElement = document.getElementById('streamer-words');
+  if (key === streamerCharacters[0]) {
+    streamerCompletedCharacters += streamerCharacters[0];
+    streamerCharacters.shift();
+    streamerWords = streamerWords.substr(1);
+
+    const updatedWords = `<span class="correct">${streamerCompletedCharacters}</span>${streamerWords}`;
+
+    streamerWordsElement.innerHTML = updatedWords;
+    streamerDone = streamerWords.length === 0;
+  } else {
+    const updatedWords = `<span class="correct">${streamerCompletedCharacters}</span><span class="incorrect">${streamerWords.substr(0, 1)}</span>${streamerWords.substr(1)}`;
+    streamerWordsElement.innerHTML = updatedWords;
+  }
 }
